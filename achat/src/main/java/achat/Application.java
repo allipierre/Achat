20bc86addce3b032c6d418775a738ccab0453647
allipierre <yottiallipierre40@yahoo.fr>
2017-07-04 00:35:13 +0200

    Configuration for Spring Project

---
+++ achat/src/main/java/achat/Application.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package achat;
+
+/**
+ * @author yotti
+ *
+ */
+public class Application {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
